d6ecd5c0b6ef09c39fe2cc698ab547f65a67b504
JohnCMeyer <JohnCMeyer232@gmail.com>
2022-04-24 19:03:53 -0700

    Add Address to Test Data

--- InsertTestData.sql
+++ InsertTestData.sql
@@ -6,8 +6,8 @@ INSERT INTO ScreenType(ModelNumber, screenType, resolution, aspectRatio)
 	VALUES('0001', 'LCD', '320x480', '1.5:1');
 INSERT INTO Specs(ModelNumber, CPU, color, weight, batteryLife, dimensions, Storage)
 	VALUES('0001', 'ARM11', 'Black', '135g', '8h', '115x61x11.6mm', '4GB');
-INSERT INTO Store(StoreNameID, StoreName, City, State, Price)
-	VALUES('01253', 'Best Buy', 'Tempe', 'Arizona', 1000);
+INSERT INTO Store(StoreNameID, StoreName, Address, City, State, Price)
+	VALUES('01253', 'Best Buy', '500 E University', 'Tempe', 'Arizona', 1000.0);
 INSERT INTO SoldAt(ModelNumber, StoreNameID)
 	VALUES('0001', '01253');
 INSERT INTO PhoneFinder(UserID, UserName, Password)
